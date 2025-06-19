543e8ad5d7a8ffb2a02f86c8e6c0d9cb9417139e
yuu-sky20 <yuusky20th@gmail.com>
2025-06-19 16:50:23 +0900

    Add floating particle effects

--- src/main/java/com/ysk/stepmove/event/tracker/PlayerTeleportTracker.java
+++ src/main/java/com/ysk/stepmove/event/tracker/PlayerTeleportTracker.java
@@ -1,14 +1,24 @@
 package com.ysk.stepmove.event.tracker;
 
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 
+import net.minecraft.particle.DustColorTransitionParticleEffect;
+
+import java.awt.*;
 import java.util.*;
 
 public class PlayerTeleportTracker {
     // 高度を記録しておく（UUID → y座標）
     public static final Map<UUID, PlayerState> hoveringPlayers = new HashMap<>();
+
+    private static final DustColorTransitionParticleEffect particle = new DustColorTransitionParticleEffect(
+            Color.MAGENTA.getRed(),
+            Color.CYAN.getBlue(),
+            1.0f
+    );
 
     public static void tick(ServerWorld world) {
         Iterator<Map.Entry<UUID, PlayerState>> it = hoveringPlayers.entrySet().iterator();
@@ -23,7 +33,7 @@ public class PlayerTeleportTracker {
                 continue;
             }
 
-            if (player.isOnGround() || player.isSwimming() || player.isClimbing() || player.isSneaking() || state.isPlayerMoved(player.getPos())) {
+            if (player.isSwimming() || player.isClimbing() || state.isPlayerMoved(player.getPos())) {
                 player.setNoGravity(false);
                 player.velocityModified = true;
                 it.remove();
@@ -33,6 +43,14 @@ public class PlayerTeleportTracker {
             player.fallDistance = 0.0F;
 
             state.lastPos = player.getPos();
+
+            world.spawnParticles(
+                    particle,
+                    player.getX(), player.getY() - 0.25, player.getZ(),
+                    10,
+                    0.4, 0.12, 0.4,
+                    1.50
+            );
         }
     }
 

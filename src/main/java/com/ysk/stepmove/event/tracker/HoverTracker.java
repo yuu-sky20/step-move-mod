c5b68fe77ea4011a73a1632ff6595f6660ed0e5c
yuu-sky20 <yuusky20th@gmail.com>
2025-06-21 14:08:50 +0900

    Split the process of playing sound effects from HoverTracker into SoundEffectPlayer

--- src/main/java/com/ysk/stepmove/event/tracker/HoverTracker.java
+++ src/main/java/com/ysk/stepmove/event/tracker/HoverTracker.java
@@ -1,6 +1,7 @@
 package com.ysk.stepmove.event.tracker;
 
 import com.ysk.stepmove.event.model.HoverState;
+import com.ysk.stepmove.event.util.SoundEffectPlayer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
@@ -10,9 +11,11 @@ import net.minecraft.particle.DustColorTransitionParticleEffect;
 import java.awt.*;
 import java.util.*;
 
+// TODO トラッキング処理と、副作用を持った状態管理を分離する
+
 public class HoverTracker {
     // 高度を記録しておく（UUID → y座標）
-    public static final Map<UUID, HoverState> hoveringPlayers = new HashMap<>();
+    private static final Map<UUID, HoverState> hoveringPlayers = new HashMap<>();
 
     private static final DustColorTransitionParticleEffect particle = new DustColorTransitionParticleEffect(
             Color.MAGENTA.getRed(),
@@ -33,14 +36,17 @@ public class HoverTracker {
                 continue;
             }
 
-            if (player.isSwimming() || player.isClimbing() || state.isPlayerMoved(player.getPos())) {
+            if (state.isPlayerMoved(player.getPos())) {
+                // TODO ホバー状態の確認
+                // TODO 排他制御
+
+
                 player.setNoGravity(false);
                 player.velocityModified = true;
                 it.remove();
+                SoundEffectPlayer.playDetachSound(player);
                 continue;
             }
-
-            player.fallDistance = 0.0F;
 
             state.setLastPos(player.getPos());
 
@@ -55,9 +61,15 @@ public class HoverTracker {
     }
 
     public static void startHovering(ServerPlayerEntity player) {
+        // TODO マネージャーでホバリングの状態確認
+
         player.setNoGravity(true);
         player.setVelocity(Vec3d.ZERO);
+        player.setPos(player.getX(), player.getY(), player.getZ());
+        player.fallDistance = 0.0F;
         player.velocityModified = true;
+        SoundEffectPlayer.playTransportSound(player);
+
         hoveringPlayers.put(player.getUuid(), new HoverState(player.getPos()));
     }
 
---
+++ src/main/java/com/ysk/stepmove/event/util/SoundEffectPlayer.java
@@ -0,0 +1,30 @@
+package com.ysk.stepmove.event.util;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import org.jetbrains.annotations.NotNull;
+
+public class SoundEffectPlayer {
+    public static void playTransportSound(@NotNull ServerPlayerEntity player) {
+        ServerWorld world = (ServerWorld) player.getWorld();
+        world.playSound(
+                null,
+                player.getX(), player.getY(), player.getZ(),
+                SoundEvents.BLOCK_PORTAL_TRAVEL,
+                SoundCategory.PLAYERS,
+                1.0f, 1.0f
+        );
+    }
+    public static void playDetachSound(@NotNull ServerPlayerEntity player) {
+        ServerWorld world = (ServerWorld) player.getWorld();
+        world.playSound(
+                null,
+                player.getX(), player.getY(), player.getZ(),
+                SoundEvents.BLOCK_PORTAL_AMBIENT,
+                SoundCategory.PLAYERS,
+                1.0f, 1.0f
+        );
+    }
+}

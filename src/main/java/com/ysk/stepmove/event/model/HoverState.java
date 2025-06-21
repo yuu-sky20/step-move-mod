6fd3159b5b0ef3655305016447cacb21acfc116b
yuu-sky20 <yuusky20th@gmail.com>
2025-06-21 19:20:09 +0900

    refactored

--- src/main/java/com/ysk/stepmove/event/model/HoverState.java
+++ src/main/java/com/ysk/stepmove/event/model/HoverState.java
@@ -3,16 +3,20 @@ package com.ysk.stepmove.event.model;
 import net.minecraft.util.math.Vec3d;
 
 public class HoverState {
+    // 最後に記録したプレイヤーの位置
     private Vec3d lastPos;
 
+    // 最終位置を更新
     public void setLastPos(Vec3d lastPos) {
         this.lastPos = lastPos;
     }
 
+    // コンストラクタ（初期位置を記録）
     public HoverState(Vec3d lastPos) {
         this.lastPos = lastPos;
     }
 
+    // プレイヤーが移動したかどうか判定
     public boolean isPlayerMoved(Vec3d nowPos) {
         return !lastPos.equals(nowPos);
     }
